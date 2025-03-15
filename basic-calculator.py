5948ec298d18e1455e743271e03c0885d29e2fa8
Christina Katsouli <christina.katsouli14@gmail.com>
2025-03-14 22:26:45 -0500

    Completed version, done deal

---
+++ basic-calculator.py
@@ -0,0 +1,25 @@
+# Ask user for input a (1st number)
+# Ask user for input b (2nd number)
+# Ask what they would like to do with the two numbers (a * b, or a + b, blablabla (add as many as possible))
+
+# Make an if statement, based on what action they want to do (modify in place)
+
+# Print the outputted number
+
+num1 = int(input("I can help you calculate whatever you want. Give me your first number: "))
+num2 = int(input("Give me your second number: "))
+
+command = input("What basic function would you like to execute on these two numbers (multiply, divide, add, subtract or power)? ")
+
+if command.lower() == "multiply":
+    result = num1 * num2
+elif command.lower() == "divide":
+    result = num1 / num2
+elif command.lower() == "add":
+    result = num1 + num2
+elif command.lower() == "subtract":
+    result = num1 - num2
+elif command.lower() == "power":
+    result = num1 ** num2
+
+print(result)
