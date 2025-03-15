7d0a820448cd4a9cc65df243a1cc441cf93f3cf0
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-03-14 22:18:40 -0500

    Create persistent-calculator.py

---
+++ persistent-calculator.py
@@ -0,0 +1,7 @@
+# Similar to the last exercise where we built a calculator, users will be able to input what type of mathematical operation they want to perform.
+# However, now, before we begin the code we will ask them to input a number (let's call it x). This number will be modified throughout the running.
+
+# Then we will open a loop (while True:) -> Every iteration of the loop we will prompt the user to input how they would like to modify x
+# (e.g add y, multiply by y, subtract y from x, divide by y) -> then update x in place, so we can keep looping and modify it
+
+# Enjoy coding 😘
